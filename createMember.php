5af154ec9b4b63f83ed66b71c1e5849838550ae5
YongJoon Kwak <yjkwak.es@esgroup.net>
2021-04-08 13:40:53 +0900

    vsc source control test

--- createMember.php
+++ createMember.php
@@ -1,39 +1,37 @@
 <?php
-include_once "/ESGROUP/PHPSever/test/class/Admin.php";
+include_once __DIR__ . "/class/Admin.php";
 
 $ID = $_POST['ID'];
 $PW = $_POST['PW'];
 ?>
 
 <script>
-    <?php if (!$ID) : ?>
+    <? if (!$ID) : ?>
         alert('None ID');
-    <?php endif; ?>
-    <?php if (!$PW) : ?>
+        history.go(-1)
+    <? endif; ?>
+
+    <? if (!$PW) : ?>
         alert('None PW')
-    <?php endif; ?>
-    history.go(-1)
+        history.go(-1)
+    <? endif; ?>
 </script>
 
-<?php
-
+<?
 if (!$ID || !$PW) {
     exit;
 }
 
 $mem = new Admin();
 $result = $mem->createMember($ID, $PW);
-
 ?>
 
-<? if (!$result) : ?>
-    <script>
+<script>
+    <? if (!$result) : ?>
         alert('already have ID')
         history.go(-1)
-    </script>
-<?php else : ?>
-    <script>
+    <? else : ?>
         alert('Create Success')
         top.location.href = "login_form.html"
-    </script>
-<?php endif; ?>
+    <? endif; ?>
+</script>
